8cf2d89afd56be6e6814270d387e9979df0ee05d
artem.v.rusanov <artem.v.rusanov@gmail.com>
2014-12-11 15:26:32 +0000

    вьюшка Карусель для фото

---
+++ T034/ViewModel/CarouselViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace T034.ViewModel
+{
+    public class CarouselViewModel
+    {
+        public string CarouselId 
+        {
+            get { return Folder.Replace("/", "_"); }
+        }
+
+        public string Folder { get; set; }
+        
+        public string Header { get; set; }
+        
+        public IEnumerable<string> Files { get; set; }
+    }
+}
