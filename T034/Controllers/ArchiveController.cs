1f1bb371b87ddd550fb41f59c71c0f324940075b
artem.v.rusanov <artem.v.rusanov@gmail.com>
2015-01-24 17:41:46 +0000

    архив

---
+++ T034/Controllers/ArchiveController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T034.ViewModel;
+
+namespace T034.Controllers
+{
+    public class ArchiveController : Controller
+    {
+        public ActionResult Person(int personId)
+        {
+            var model = new PersonViewModel();
+
+            if (personId == 1)
+            {
+                model.FullName =
+                    "Карташова Клавдия Петровна - ветеран Великой Отечественной войны, преподаватель, и.о. директора, заведующая Сталинградской-Волгоградской школы, училища культуры";
+                model.Docs = new CarouselViewModel("/Content/images/people/kpk/recollections/",
+                                                   Server.MapPath("/Content/images/people/kpk/recollections/"),
+                                                   "Воспоминания Клавдии Петровны", "");
+            }
+            if (personId == 2)
+            {
+                model.FullName =
+                    "Переходник Борис Григорьевич - участник Великой Отечественной войны, преподаватель, зав.отделением Сталинградской-Волгоградской школы, училища культуры, заслуженный работник культуры РСФСР";
+                model.Docs = new CarouselViewModel("/Content/images/people/perehodnik/",
+                                                   Server.MapPath("/Content/images/people/perehodnik/"),
+                                                   "Документы Бориса Григорьевича", "");
+            }
+            return View(model);
+        }
+    }
+}
--- T034/ViewModel/CarouselViewModel.cs
+++ T034/ViewModel/CarouselViewModel.cs
@@ -6,7 +6,7 @@ namespace T034.ViewModel
 {
     public class CarouselViewModel
     {
-        public CarouselViewModel(string folder, string path, string header)
+        public CarouselViewModel(string folder, string path, string header, string interval = "5000")
         {
             var directory = new DirectoryInfo(path);
             var files = directory.GetFiles().Select(f => f.Name);
@@ -14,6 +14,7 @@ namespace T034.ViewModel
             Header = header;
             Files = files;
             Folder = folder;
+            Interval = interval;
         }
 
         public string CarouselId 
@@ -24,6 +25,8 @@ namespace T034.ViewModel
         public string Folder { get; set; }
         
         public string Header { get; set; }
+        
+        public string Interval { get; set; }
         
         public IEnumerable<string> Files { get; set; }
     }
