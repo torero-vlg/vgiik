31121637cc39995ae6ab1b535f7f2803eb3b09e4
Артём <artem.v.rusanov@gmail.com>
2016-02-17 18:43:50 +0300

    содержимое сайта

--- T034/Controllers/HomeController.cs
+++ T034/Controllers/HomeController.cs
@@ -41,33 +41,38 @@ namespace T034.Controllers
             //string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~"));
 
             var directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/dpi/"));
-            var files = directory.GetFiles().Select(f => f.Name);
+
+            var files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var dpi = files.Select(file => new NodeViewModel {Path = "/Content/images/photo/dpi/" + file, Description = ""});
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/staropoltavka/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var staropoltavka = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/staropoltavka/" + file, Description = "" });
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/24012015/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var nodes = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/24012015/" + file, Description = "" });
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/summer2012/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var summer2012 = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/summer2012/" + file, Description = "" });
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/autumn2012/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var autumn2012 = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/autumn2012/" + file, Description = "" });
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/motorship2013/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var motorship2013 = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/motorship2013/" + file, Description = "" });
 
             directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/13082015/"));
-            files = directory.GetFiles().Select(f => f.Name);
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
             var photo13082015 = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/13082015/" + file, Description = "" });
 
+			directory = new DirectoryInfo(Server.MapPath("/Content/images/photo/traditional/"));
+            files = directory.Exists ? directory.GetFiles().Select(f => f.Name) : new List<string>();
+            var traditional = files.Select(file => new NodeViewModel { Path = "/Content/images/photo/traditional/" + file, Description = "" });
+			
             var model = new List<CarouselViewModel>
                 {
                     new CarouselViewModel(dpi, "Кафедра ДПИ", ""),
@@ -76,7 +81,8 @@ namespace T034.Controllers
                     new CarouselViewModel(summer2012, "Лето - 2012", ""),
                     new CarouselViewModel(autumn2012, "Осень - 2012", ""),
                     new CarouselViewModel(motorship2013, "Теплоход - 2013", ""),
-                    new CarouselViewModel(photo13082015, "", "")
+                    new CarouselViewModel(photo13082015, "", ""),
+					new CarouselViewModel(traditional, "Кафедра традиционной культуры", "")
                 };
 
             return View(model);
