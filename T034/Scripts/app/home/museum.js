596558456d63445d51e47e36f5f5e57d712aa90f
Артём <artem.v.rusanov@gmail.com>
2016-04-15 21:13:03 +0300

    requireJS скрипты на страницах отображения - без админки

---
+++ T034/Scripts/app/home/museum.js
@@ -0,0 +1,35 @@
+﻿define([], function () {
+
+    return {
+        Initialize: function () {
+
+            $('#personModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget); // Button that triggered the modal
+                var personid = button.data('personid'); // Extract info from data-* attributes
+
+
+                $.ajax({
+                    url: "/Archive/Person?personId=" + personid,
+                    cache: false
+                })
+                .done(function (html) {
+                    $("#modalBody").html(html);
+                });;
+            });
+
+            $('#bookModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget); // Button that triggered the modal
+                var id = button.data('id'); // Extract info from data-* attributes
+
+                $.ajax({
+                    url: "/Museum/Publication?id=" + id,
+                    cache: false
+                })
+                    .done(function (html) {
+                        $("#modalBody").html(html);
+                    });;
+            });
+
+        }
+    }
+});
