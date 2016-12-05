0df46bd144061ef52ed2b1f7493d192004da8967
Артём <artem.v.rusanov@gmail.com>
2016-12-05 19:58:36 +0300

    новая таблица webpermission

---
+++ T034.Api/Entity/Administration/WebPermissionRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T034.Api.Entity.Administration
+{
+    /// <summary>
+    /// Разрешение, содержащееся в роли
+    /// </summary>
+    public class WebPermissionRole : Entity
+    {
+        public virtual string Name { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
