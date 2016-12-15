55dfd79f752e1d679e53f7ab67d5a233109c93c0
Артём <artem.v.rusanov@gmail.com>
2016-12-15 18:54:50 +0300

    добавил сущности файлы и папки

---
+++ T034.Api/Entity/Files.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using T034.Api.Entity.Administration;
+
+namespace T034.Api.Entity
+{
+    public class Files : Entity
+    {
+        public virtual string Name { get; set; }
+        public virtual int DownloadCounter { get; set; }
+        
+        public virtual Folder Folder { get; set; }
+        
+        public virtual DateTime LogDate { get; set; }
+        public virtual int Size { get; set; }
+        public virtual User User { get; set; }
+    }
+}
